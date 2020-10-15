bc41472264ae11ee232caa1f538a8584ddad2f1a
Kamil Olszewski <kamil.web@gmail.com>
2020-10-15 21:45:39 +0200

    add key controls

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         """Starting the main game loop"""
         while True:
             self._check_events()
+            self.rocket.update()
             self._update_screen()
 
     def _check_events(self):
@@ -28,6 +29,17 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.rocket.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.rocket.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.rocket.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.rocket.moving_left = False
 
     def _update_screen(self):
         """Updates the images on the screen and goes to a new screen"""
--- rocket.py
+++ rocket.py
@@ -16,6 +16,17 @@ class Rocket:
         # Each new rocket appears at the bottom of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Options that indicate the movement of the rocket
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the position of the rocket based on the option that indicates its movement"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         """Display of the rocket in its active position"""
         self.screen.blit(self.image, self.rect)
